36090e39b13547fa7ddc6f1121eedcf975654ed3
wallins@1 <collins.wamba@codeautcam.com>
2024-03-13 17:03:25 +0100

    Add second spider for pagej

--- callCenter/callCenter/spiders/pagej2.py
+++ callCenter/callCenter/spiders/pagej2.py
@@ -1,26 +1,12 @@
-import re
-import time
 import scrapy
-import random
-from bs4 import BeautifulSoup
-from scrapy.http import HtmlResponse
-from selenium import webdriver
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-from rules.actions import Actions
-#Selenium stealth -- https://www.zenrows.com/blog/selenium-stealth#scrape-with-stealth
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager #https://pypi.org/project/webdriver-manager/
-from selenium_stealth import stealth
-class PagejSpider(scrapy.Spider):
-    name = "pagej"
-    allowed_domains = ["google.com"]
-    start_urls = ["https://www.google.com/"]
-    base_url = "https://www.pagesjaunes.fr"
-    old_url = ""
+
+
+class Pagej2Spider(scrapy.Spider):
+    name = "pagej2"
+    allowed_domains = ["www.pagesjaunes.fr"]
+    start_urls = ["https://www.pagesjaunes.fr/"]
+    base_url = "https://wwnnuaire.bt/chercherlespros?quoiqui=confiserie%20chocolaterie&ou=Auvergne-Rh%C3%B4ne-Alpes&idOu=R84&page={}"
+    num_pages = 27
     user_agents = [
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/118.0.2088.88',
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
@@ -30,7 +16,7 @@ class PagejSpider(scrapy.Spider):
         ]
     user_agent = random.choice(user_agents)
     def __init__(self, *args, **kwargs):
-        super(PagejSpider, self).__init__(*args, **kwargs)
+        super(Pagej2Spider, self).__init__(*args, **kwargs)
         print('🚀  Starting the engine...')
         self.run_stealth()
     def run_stealth(self):
@@ -67,83 +53,13 @@ class PagejSpider(scrapy.Spider):
         options = self.driver.options
         options.add_argument(f'--proxy-server={proxy}')
         options.add_argument(f'user-agent={self.user_agent}')
-    
-    def parse(self, response, next_page=None):
-        print('🕸️  Parsing')
-        if next_page is None: 
-            self.driver.get("https://www.pagesjaunes.fr/annuaire/chercherlespros?quoiqui=confiserie%20chocolaterie&ou=Auvergne-Rh%C3%B4ne-Alpes&idOu=R84&page=27&contexte=URtUXxYXKPVaBxD8bcbeU0g0%2BqzfqeWdpnMQ/Se9ERqxz4RB632llrOwgUTczExoGsJs1VyKmWI5ZQ3A4TOLRXr%2BO9rHXeu3sLV3uJ5x3f%2BLl3bP16XyIlwAaS7duP8i%2BDTbPMkSxXMGjbxvuwGccWRu7fpZOFTR/8BRn2Sj9XwejyKbZtEK2Yk0WMv2JIPYWIVrksDFbVPEaHK3BCv49/Qm%2BbRmUIJUfe%2B6Y1H7U1L4U2oR0jEh6VFYNQiGEj7l&quoiQuiInterprete=confiserie%20chocolaterie")
-        else:
-            self.driver.quit()
-            time.sleep(random.randint(2, 5))
-            self.run_stealth()
-            print('Getting Next page')
-            self.driver.get(next_page)
-        self.driver.save_screenshot("opensea.png")
-        time.sleep(random.randint(2, 5))
-        self.old_url = self.driver.current_url
-        website = self.driver.page_source
-        results = BeautifulSoup(website, 'html.parser')
-        hotel_links = results.select('.bi-content a[href*="pros/"]')
-        # hotel_links = results.select('.results a[href*="pros/"]')
-        if hotel_links:
-            print('Len of Hotel Links: ', len(hotel_links))
-            for link in hotel_links:
-                # link.find("a",{"class":"bi-denomination"}).get('href')
-                href = link.get('href')
-                target_url = self.base_url + href
-                print(target_url)
-                time.sleep(random.randint(2, 5))
-                self.driver.get(target_url)
-                time.sleep(random.randint(5, 15))
-                html_content  = self.driver.page_source
-                html_response = HtmlResponse(self.driver.current_url, body=html_content, encoding='utf-8')
-                scroll_port = random.randint(300, 800)
-                Actions.scroll_page('down', scroll_port)
-                time.sleep(random.uniform(0.6, 1.5))
-                yield from self.scrape_content(html_response)
-        print(self.driver.execute_script("return navigator.userAgent;"))
-        time.sleep(random.uniform(0.6, 1.5))
-        self.driver.get(self.old_url)
-        time.sleep(random.uniform(0.5, 2.5))
-        # Check for Pagination
-        while True:
-            try:
-                wait = WebDriverWait(self.driver, 15)
-                wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "a#pagination-next")))
-            except:
-                self.driver.refresh()
-            
-            time.sleep(random.uniform(0.5, 1.5))
-            try:
-                next_page_link = self.driver.find_element(By.CSS_SELECTOR, "a#pagination-next")
-            except NoSuchElementException:
-                print('NO NEXT PAGE FOUND')
-            try:
-                popup = self.driver.find_element(By.ID, 'didomi-notice-agree-button')
-                time.sleep(random.uniform(0.5, 1.5))
-                ActionChains(self.driver)\
-                    .click(popup)\
-                    .perform()
-            except:
-                pass
-            
-            if next_page_link:
-                time.sleep(random.uniform(0.5, 2.7))
-                ActionChains(self.driver)\
-                    .move_to_element_with_offset(next_page_link, random.randint(2, 12), random.randint(0, 9))\
-                    .perform()
-                
-                
-                ActionChains(self.driver)\
-                    .click(next_page_link)\
-                    .perform()
-                
-                
-                current_url = self.driver.current_url  
-                print('Current URL', current_url)
-                yield from self.parse(response, current_url)
-            else:
-                break
+
+
+    def parse(self, response):
+        for page_num in range(1, self.num_pages + 1):
+
+            url = self.base_url.format(page_num)
+
     def scrape_content(self, response_data):
         results = BeautifulSoup(response_data.body, 'html.parser')
         try:
@@ -198,4 +114,3 @@ class PagejSpider(scrapy.Spider):
             # 'Tariffs': all_tariffs
         }
         yield data
-         

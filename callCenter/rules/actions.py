cb5e6d99046a0eafcb8721097f95e6a21923441e
wallins@1 <collins.wamba@codeautcam.com>
2024-03-19 16:28:28 +0100

    pagej2 data inconsistent

--- callCenter/rules/actions.py
+++ callCenter/rules/actions.py
@@ -17,6 +17,9 @@ actions = [
 
 # Define a function for each action
 class Actions:
+    def __init__(self, driver):
+        self.driver = driver
+        
     def open_url(self, url):
         self.driver.get(url)
 
